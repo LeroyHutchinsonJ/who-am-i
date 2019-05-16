9c4ba72eecbad652bf052ee04584c652d5154b35
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-16 11:29:24 -0400

    Component Did Mount
    Used the component did mount function asynchronously to get and set the data using the fetch api

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+//Program that takes a persons face and name then puts it on the screen
 class App extends React.Component {
+  state = {
+    personArr: [],
+    value: 9
+  };
+
+  componentDidMount = async () => {
+    //Put the url in a var
+    var url = "https://randomuser.me/api/";
+
+    //fetch data from the url
+    var response = await fetch(url);
+
+    //turn data into json
+    var json = await response.json();
+
+    this.setState({ personArr: json.results });
+  };
   render() {
+    var { personArr, value } = this.state;
+
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        {personArr.map(person => {
+          return <div key={value}>Hi! My name is {person.gender} </div>;
+        })}
       </div>
     );
   }
